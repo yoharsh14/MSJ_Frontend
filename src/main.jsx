07a373f34faa83fd74e9092b98e439c3f6a1d080
yoharsh14 <86423823+yoharsh14@users.noreply.github.com>
2024-10-19 02:37:47 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import { store } from "./redux/Store.js";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       {/* <Provider store={store}> */}
-        <App />
+      <App />
       {/* </Provider> */}
     </BrowserRouter>
   </StrictMode>
